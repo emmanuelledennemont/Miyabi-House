eaf0e60e234f21303b86268c17df1265b71bd6de
Emmanuelle  Dennemont <emmanuelledennemont@macbook-pro-de-emmanuelle.home>
2023-10-19 23:13:19 +0200

    ajout images

--- src/ui/modules/landing-page/components/hero-top/hero-top-view.tsx
+++ src/ui/modules/landing-page/components/hero-top/hero-top-view.tsx
@@ -14,6 +14,10 @@ const heroContent = {
     img1: '/assets/svg/hero.svg',
     img2: '/assets/svg/Group 20.svg',
     img3: '/assets/svg/Group 8.svg',
+    img4: '/assets/svg/fondjapon1 1.svg',
+    img5: '/assets/svg/ramen1 1.svg',
+    img6: '/assets/svg/sushi2 1.svg',
+    img7: '/assets/svg/mochi1 1.svg',
   },
 };
 
@@ -105,12 +109,12 @@ export const HeroTopView = () => {
           <Image
             src={heroContent.image.img2}
             alt="illustration soupe ramen illustration sushi baguettes et spécialités japonaises"
-            width={290}
+            width={300}
             height={290}
-            className="object-cover absolute top-[-10px] left-[-110px] -z-40"
+            className="object-cover absolute top-[320px] left-[-110px] -z-40"
             priority
           />
-          <div className="lg:flex items-center justify-between gap-[20px] ">
+          <div className="lg:flex justify-between gap-[20px] ">
             <div className="lg:w-6/12 mb-10 lg:mb-0">
               <Typography
                 variant="h1"
@@ -129,7 +133,23 @@ export const HeroTopView = () => {
               <Button size="lg" variant="secondary" className="mx-auto">
                 {heroContent.text.button}
               </Button>
+
+              <div className="lg:w-2/12 space-y-2 absolute bottom-20 left-[490px]">
+                {heroContent.image.img1 && (
+                  <Image
+                    src={heroContent.image.img1}
+                    alt="futur video"
+                    width={90}
+                    height={96}
+                    className="h-full w-full max-w-xl p-3  bg-beigebg-200 rounded rounded-4xl"
+                    priority
+                  />
+                )}
+              </div>
+
             </div>
+
+
             <div className="lg:w-6/12 space-y-2">
               <div className="flex space-x-2 items-stretch justify-end">
                 <div className="w-7/12">
@@ -146,21 +166,21 @@ export const HeroTopView = () => {
                 </div>
                 <div className="w-4/12 self-end space-y-2">
                   <div className="grid grid-cols-2 gap-2">
-                    {heroContent.image.img3 && (
+                    {heroContent.image.img5 && (
                       <Image
-                        src={heroContent.image.img3}
+                        src={heroContent.image.img5}
                         alt="illustration sushi baguettes et spécialités japonaises"
-                        width={230}
+                        width={290}
                         height={230}
-                        className="object-cover h-full w-full bg-primary-100/80 p-3 rounded rounded-2xl"
+                        className="object-cover h-full w-full bg-beigebg-200 p-2 rounded rounded-2xl"
                         priority
                       />
                     )}
-                    <div className="bg-secondary/80 rounded-tr-[200px] rounded-2xl"></div>
+                    <div className="bg-secondary/60 rounded-tr-[200px] rounded-2xl"></div>
                   </div>
-                  {heroContent.image.img3 && (
+                  {heroContent.image.img4 && (
                     <Image
-                      src={heroContent.image.img3}
+                      src={heroContent.image.img4}
                       alt="illustration sushi baguettes et spécialités japonaises"
                       width={380}
                       height={380}
@@ -183,10 +203,10 @@ export const HeroTopView = () => {
               <div className="flex space-x-2 items-stretch justify-end">
                 <div className="w-4/12">
                   <div className="grid grid-cols-2 gap-2">
-                    <div className="bg-secondary/80 rounded-bl-[200px] rounded-2xl"></div>
-                    {heroContent.image.img3 && (
+                    <div className="bg-secondary/60 rounded-bl-[200px] rounded-2xl"></div>
+                    {heroContent.image.img7 && (
                       <Image
-                        src={heroContent.image.img3}
+                        src={heroContent.image.img7}
                         alt="illustration sushi baguettes et spécialités japonaises"
                         width={230}
                         height={230}
@@ -196,19 +216,20 @@ export const HeroTopView = () => {
                     )}
                   </div>
                 </div>
-                <div className="w-7/12 h-[130px]">
-                  {heroContent.image.img1 && (
+                <div className="w-2/12">
+                  {heroContent.image.img6 && (
                     <Image
-                      src={heroContent.image.img3}
+                      src={heroContent.image.img6}
                       alt="illustration sushi baguettes et spécialités japonaises"
-                      width={230}
-                      height={230}
-                      className="object-cover h-full w-full bg-primary-100/80 p-3 rounded rounded-2xl"
+                      width={220}
+                      height={200}
+                      className="object-cover max-h-full max-w-full bg-beigebg-200  p-0 rounded rounded-2xl"
                       priority
                     />
-
                   )}
+
                 </div>
+
               </div>
             </div>
           </div>
