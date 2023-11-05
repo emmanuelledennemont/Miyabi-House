ecb522421116c9bc6acfc66841461c0dfbc78fc5
Emmanuelle  Dennemont <emmanuelledennemont@orange.fr>
2023-11-05 01:02:48 +0100

    hauteur max de la card

--- src/ui/modules/landing-page/components/specialities/specialities-view.tsx
+++ src/ui/modules/landing-page/components/specialities/specialities-view.tsx
@@ -14,25 +14,25 @@ interface SpecialitiesInterface {
 
 const specialitiesData: SpecialitiesInterface[] = [
   {
-    img: '/assets/svg/fondpromo 1.svg',
+    img: '/assets/svg/Group 10.svg',
     alt: 'illustration caligraphie japonaise',
     title: 'Sushi',
     varieties: '12 varieties'
   },
   {
-    img: '/assets/svg/fondpromo 1.svg',
+    img: '/assets/svg/Group 15.svg',
     alt: 'illustration caligraphie japonaise',
     title: 'Mochi',
     varieties: '4 varieties'
   },
   {
-    img: '/assets/svg/fondpromo 1.svg',
+    img: '/assets/svg/Group 16.svg',
     alt: 'illustration caligraphie japonaise',
     title: 'Ramen',
     varieties: '7 varieties'
   },
   {
-    img: '/assets/svg/fondpromo 1.svg',
+    img: '/assets/svg/Group 8 (1).svg',
     alt: 'illustration caligraphie japonaise',
     title: 'Bubble Tea',
     varieties: '5 varieties'
@@ -43,14 +43,16 @@ export const SpecialitiesPart = () => {
   const specialitiesList = specialitiesData.map((speciality) => (
 
     <Card key={uuidv4()} size="sm" variant='beigebg'>
-      <Image
-        src={speciality.img}
-        alt={speciality.alt}
-        width={30}
-        height={30}
-        className="object-cover w-full"
-        priority
-      />
+      <div className="flex items-center justify-center h-[200px] max-h-60 m-8">
+        <Image
+          src={speciality.img}
+          alt={speciality.alt}
+          width={230}
+          height={230}
+          className="object-cover  max-h-full max-w-full m-6 "
+          priority
+        />
+      </div>
       <div className="flex justify-between px-6 mb-6">
         <Typography variant="lead" component="h3" weight="medium">
           {speciality.title}
@@ -60,8 +62,8 @@ export const SpecialitiesPart = () => {
         </Typography>
       </div>
 
-      <div className="flex justify-center mb-6 w-full">
-        <Button variant="beigebg" className="w-full" href="/">
+      <div className="flex justify-center mt-5 mb-6 w-full">
+        <Button size="lg" variant="beigebg" className="w-full" href="/">
           Order Now
         </Button>
       </div>
@@ -70,7 +72,7 @@ export const SpecialitiesPart = () => {
 
   return (
     <Container className='py-20 relative mt-10'>
-      <div className="grid grid-cols-12 grid-row-2 gap-x-24 gap-y-6 py-6">
+      <div className="grid grid-cols-12 grid-row-2 gap-x-16 gap-y-6 py-6">
         <div className="row-span-2 grid grid-cols-2 col-span-7 gap-6">
           {specialitiesList}
         </div>
